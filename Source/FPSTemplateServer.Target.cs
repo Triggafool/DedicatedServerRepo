2fc25b1c79ee7220d0101af21e04b14a98ab4764
Triggafool <Triggafool@gmail.com>
2025-02-07 00:33:07 -0500

    First Commit

---
+++ Source/FPSTemplateServer.Target.cs
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+using System;
+
+public class FPSTemplateServerTarget : TargetRules
+{
+    public FPSTemplateServerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        ExtraModuleNames.Add("FPSTemplate");
+        ExtraModuleNames.Add("DedicatedServers");
+    }
+}
